e13729a890a01a93424682141a3fe094f514549d
srikar <dhanusrikar@gmail.com>
2024-02-11 20:19:38 +0530

    Changing the model object name
    We are changing the model object name which is shown in the django admin. We create a __str__(self) function in the Task class and return the title of the object.

--- todoList/home/models.py
+++ todoList/home/models.py
@@ -5,3 +5,6 @@ class Task(models.Model):
     taskTitle = models.CharField(max_length=30)
     taskDesc = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.taskTitle
